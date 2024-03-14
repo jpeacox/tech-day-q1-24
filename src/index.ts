9a75d2653d1930ae35bd74260fb95dd74f455967
jpeacox <jcox@jhancock.com>
2024-03-14 12:45:46 -0400

    Kickstart running natively a bit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+#!/usr/bin/env bun
 import { zli } from './zli';
 
-const app = zli().command('echo', (cmd) =>
-  cmd
-    .describe('Echoes the input')
-    .invoke(({ _ }, stdout) => stdout.write(_.join(' ')))
-);
-
+const app = zli()
+  .showHelpOnNotFound()
+  .help()
+  .command('echo', (cmd) =>
+    cmd
+      .describe('Echoes the input')
+      .invoke(({ _ }, stdout) => stdout.write(_.join(' ')))
+  );
 await app.exec(process.argv.slice(2));
